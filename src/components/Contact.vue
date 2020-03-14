268c87d2f543306f40e80d1ad9498a1f6b91b8ce
Timo Varila <timo.varila@siili.com>
2020-03-14 16:29:19 +0200

    delivery, about, how to order, history

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     <v-card>
-      <v-card-title>Contact Us</v-card-title>
+      <v-card-title>Contact</v-card-title>
+      <v-card-text>
+        <strong>Pizza Boss</strong><br>
+        <address>
+        Address line 1 <br>
+        Address line 2 <br>
+        Phone: (050) 123 4567
+        </address>
+        <address>
+          <strong>Email:</strong> <br>
+          <p>contact@pizzaboss.com</p>
+        </address>
+
+      </v-card-text>
     </v-card>
   </div>
 </template>
