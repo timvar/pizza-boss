a2c035d175fb3494b1916105e3d0243c6eaf2e1c
Timo Varila <timo.varila@siili.com>
2020-04-04 15:34:06 +0300

    menuItem card

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card
     class="mx-auto"
-    max-width="344"
+    max-width="250"
   >
     <v-img
       src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
@@ -16,18 +16,31 @@
       {{menuItem.description}}
     </v-card-subtitle>
     <v-card-text>
-      <div>Standard: {{menuItem.prices.standard}}</div>
-      <div>Family: {{menuItem.prices.family}}</div>
+        <table>
+      <thead>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="text-right">Standard:</td>
+          <td class="text-right">{{menuItem.prices.standard}} </td>
+          <td>
+            <v-btn x-large icon color="green darken-3" @click="addToBasket(pizza, option)">
+              <v-icon>mdi-plus-circle</v-icon>
+            </v-btn>
+          </td>
+        </tr>
+        <tr>
+          <td class="text-right">Family:</td>
+          <td class="text-right">{{menuItem.prices.family}} </td>
+          <td>
+            <v-btn x-large icon color="green darken-3" @click="addToBasket(pizza, option)">
+              <v-icon>mdi-plus-circle</v-icon>
+            </v-btn>
+          </td>
+        </tr>
+      </tbody>
+  </table>
     </v-card-text>
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn
-        color="purple"
-        text
-      >
-        Explore
-      </v-btn>
-    </v-card-actions>
   </v-card>
 </template>
 <script>
